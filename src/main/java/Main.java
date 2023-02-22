5ba4264cb376d46fcfb4acca5aa3dd4dcbcd7ebc
Harsh Panchal <hpanchal092@gmail.com>
2023-02-21 17:12:34 -0800

    java sucksssssss

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,37 +3,53 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        final Label label;
-        final TextField tf;
+        Label label;
+        TextField tf;
         Button button;
-        VBox vbox;
+        VBox textArea;
+        HBox inputArea;
+        VBox root;
         Scene scene;
 
         tf = new TextField("Text Field!");
-        tf.setMaxWidth(200);
+        tf.setMinWidth(500);
 
         label = new Label("Type text and click the button");
-        button = new Button("Click");
+        button = new Button("Send");
+        inputArea = new HBox(tf, button);
+        textArea = new VBox(label);
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e) {
-                label.setText(tf.getText());
+        tf.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                textArea.getChildren().add(new Label("> " + tf.getText()));
+                tf.setText("");
             }
         });
 
-        vbox = new VBox(label, tf, button);
-        vbox.setSpacing(20);
-        vbox.setAlignment(Pos.CENTER);
-        scene = new Scene(vbox, 300, 200);
+        button.setOnAction(e -> {
+            textArea.getChildren().add(new Label("> " + tf.getText()));
+            tf.setText("");
+        });
+
+        textArea.setSpacing(5);
+        textArea.setAlignment(Pos.BASELINE_LEFT);
+        inputArea.setAlignment(Pos.CENTER);
+        inputArea.setSpacing(5);
+
+        root = new VBox(textArea, inputArea);
+        root.setAlignment(Pos.BOTTOM_CENTER);
+        root.setSpacing(5);
+
+        scene = new Scene(root, 1280, 720);
 
         primaryStage.setTitle("kyle is a bozo");
         primaryStage.setScene(scene);
