2a2b256f3efe8ba6c38dcf443c8246d5f1848db8
Kyle Le <87329026+Jc2018-CC@users.noreply.github.com>
2023-03-02 19:43:17 +0000

    labeling

--- Main.java
+++ Main.java
@@ -9,14 +9,29 @@ public class Main {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         Button b = new Button("submit");
-        b.setBounds(50,30,50,30);
+        b.setBounds(40,280,50,30);
         jFrame.add(b);
         
         JLabel label = new JLabel("Slope Field Title");
         label.setText("Slope Field Generator");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.TOP);
+        label.setBounds(200,20,200,20);
         jFrame.add(label);
+
+        JLabel dytxt = new JLabel("DY");
+        dytxt.setText("DY");
+        dytxt.setBounds(20,100,50,20);
+        jFrame.add(dytxt);
+
+        JLabel dytxt = new JLabel("DY");
+        dytxt.setText("DY");
+        dytxt.setBounds(20,100,50,20);
+        jFrame.add(dytxt);
+
+        JLabel dxtxt = new JLabel("DX");
+        dxtxt.setText("DX");
+        dxtxt.setBounds(20,130,50,20);
+        jFrame.add(dxtxt);
+        //tf = new TextField("")
         
         jFrame.setVisible(true);
     }
