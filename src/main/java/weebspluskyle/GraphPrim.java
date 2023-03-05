697bc16cdab7f75d349cf3516f8d132972256219
Kyle Le <87329026+Jc2018-CC@users.noreply.github.com>
2023-03-05 07:25:52 +0000

    graphprim: a easy version

---
+++ src/main/java/weebspluskyle/GraphPrim.java
@@ -0,0 +1,8 @@
+package weebspluskyle;
+//this is gonna be my baby version with fixed windows size and shit, no scrolling or zooming
+public class Graph {
+    public static void draw(String equation) {
+        // TODO
+        System.out.println("Equation: " + equation);
+    }
+}
