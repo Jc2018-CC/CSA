f4e61fdd2781f272fa93bf949b8d9c94fb6a7f01
Kyle Le <87329026+Jc2018-CC@users.noreply.github.com>
2023-03-05 02:17:58 +0000

    now it actually runs lol - kyle

--- src/main/java/weebspluskyle/App.java
+++ src/main/java/weebspluskyle/App.java
@@ -10,34 +10,20 @@ public class App {
         jFrame.setSize(500, 360);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        Button b = new Button("submit");
-        b.setBounds(40,280,50,30);
-        jFrame.add(b);
-        
-        //b button listener
-        b.addActionListener(e -> {
-            System.out.println("Have fun!");
-            String equation = tf.getTest();
-            SlopeField.graph(equation)
-        });
-        
         JLabel label = new JLabel("Slope Field Title");
         label.setText("Slope Field Generator");
         label.setBounds(200,20,200,20);
         jFrame.add(label);
 
         JLabel dytxt = new JLabel("DY");
-        dytxt.setText("DY");
         dytxt.setBounds(20,100,50,20);
         jFrame.add(dytxt);
 
-        JLabel line = new JLabel("line");
-        line.setText("_____");
+        JLabel line = new JLabel("_____");
         line.setBounds(14,103,50,20);
         jFrame.add(line);
 
         JLabel dxtxt = new JLabel("DX");
-        dxtxt.setText("DX");
         dxtxt.setBounds(20,120,50,20);
         jFrame.add(dxtxt);
 
@@ -45,6 +31,18 @@ public class App {
         tf = new JTextField("Enter your differential equation");
         tf.setBounds(65, 105, 220, 30);
         jFrame.add(tf);
+
+        Button b = new Button("submit");
+        b.setBounds(40,280,50,30);
+        jFrame.add(b);
+        
+        //b button listener
+        b.addActionListener(e -> {
+            System.out.println("Have fun!");
+            String equation = tf.getText();
+            //SlopeField.graph(equation, xMinSet, yMinSet, xMaxSet, yMaxSet, stepSet);
+            //once ricky and harsh do backend shit
+        });
         
         jFrame.setVisible(true);
     }
@@ -55,6 +53,6 @@ public class App {
 }
 class SlopeField{
     public static void graph(String equation, int xMin, int yMin, int xMax, int yMax, double stepSize){
-        System.out.println("equation:" + equation)
+        System.out.println("equation:" + equation);
     }
 }
