3138280d421ba3da984e7ad30a1039cd94bc6835
Harsh Panchal <hpanchal092@gmail.com>
2023-03-04 18:41:07 -0800

    NEW CLASSES GO IN NEW FILES KYLE

--- src/main/java/weebspluskyle/App.java
+++ src/main/java/weebspluskyle/App.java
@@ -9,22 +9,22 @@ public class App {
         jFrame.setLayout(null);
         jFrame.setSize(500, 360);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         JLabel label = new JLabel("Slope Field Title");
         label.setText("Slope Field Generator");
-        label.setBounds(200,20,200,20);
+        label.setBounds(200, 20, 200, 20);
         jFrame.add(label);
 
         JLabel dytxt = new JLabel("DY");
-        dytxt.setBounds(20,100,50,20);
+        dytxt.setBounds(20, 100, 50, 20);
         jFrame.add(dytxt);
 
         JLabel line = new JLabel("_____");
-        line.setBounds(14,103,50,20);
+        line.setBounds(14, 103, 50, 20);
         jFrame.add(line);
 
         JLabel dxtxt = new JLabel("DX");
-        dxtxt.setBounds(20,120,50,20);
+        dxtxt.setBounds(20, 120, 50, 20);
         jFrame.add(dxtxt);
 
         JTextField tf;
@@ -33,26 +33,21 @@ public class App {
         jFrame.add(tf);
 
         Button b = new Button("submit");
-        b.setBounds(40,280,50,30);
+        b.setBounds(40, 280, 100, 30);
         jFrame.add(b);
-        
-        //b button listener
+
+        // b button listener
         b.addActionListener(e -> {
-            System.out.println("Have fun!");
             String equation = tf.getText();
-            //SlopeField.graph(equation, xMinSet, yMinSet, xMaxSet, yMaxSet, stepSet);
-            //once ricky and harsh do backend shit
+            Graph.draw(equation, 0, 0, 0, 0, 0);
+            // Graph.draw(equation, xMinSet, yMinSet, xMaxSet, yMaxSet, stepSet);
+            // once ricky and harsh do backend shit
         });
-        
+
         jFrame.setVisible(true);
     }
-    
-  public static void main(String[] args) {
-    createAndShowGUI();
-  }
-}
-class SlopeField{
-    public static void graph(String equation, int xMin, int yMin, int xMax, int yMax, double stepSize){
-        System.out.println("equation:" + equation);
+
+    public static void main(String[] args) {
+        createAndShowGUI();
     }
 }
---
+++ src/main/java/weebspluskyle/Graph.java
@@ -0,0 +1,8 @@
+package weebspluskyle;
+
+public class Graph {
+    public static void draw(String equation, int xMin, int yMin, int xMax, int yMax, double stepSize) {
+        // TODO
+        System.out.println("Equation: " + equation);
+    }
+}
