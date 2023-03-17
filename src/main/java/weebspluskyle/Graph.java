e73aa0a0c1fcebd507ebca8fa18654d6d2e071b3
Ricky Tran <126818558+RickyLTran@users.noreply.github.com>
2023-03-17 19:25:27 +0000

    Remade the draw function

--- src/main/java/weebspluskyle/Graph.java
+++ src/main/java/weebspluskyle/Graph.java
@@ -18,7 +18,7 @@ public class Graph extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawGrid(g);
-        draw(g, -5, -5, 5, 5, 15);
+        draw(g, 0, -25, 8, 15, 25);
     }
 
     // defaulted to 10 by 10 grid, remind me to make it a paremeter later
@@ -36,51 +36,33 @@ public class Graph extends JPanel {
         g.drawLine(size / 2, 0, size / 2, size);
         g.drawLine(0, size / 2, size, size / 2);
     }
+    
     private void draw(Graphics g, double xMinSet, double yMinSet, double xMaxSet, double yMaxSet, int stepSet){
         double xIncrement = (xMaxSet - xMinSet)/stepSet;
         double yIncrement = (yMaxSet - yMinSet)/stepSet;
         double xScaler = size/(xMaxSet - xMinSet);
         double yScaler = size/(yMaxSet - yMinSet);
-        double dashSize = (size/(2*stepSet))/(0.5*(xScaler+yScaler));
+        double dashSize = size / (2 * stepSet);
         double m = 0;
         for (int i = 0; i < stepSet; i++){
             for (int j = 0; j < stepSet; j++){
                 double xMid = xMinSet + (0.5 + i) * xIncrement;
                 double yMid = yMinSet + (0.5 + j) * yIncrement;
-                m = xMid*xMid-yMid;
+                m = xMid+yMid;
+                m *= (yScaler / xScaler);
+                xMid = xScaler * (xMid - xMinSet);
+                yMid = -(yScaler * (yMid - yMinSet) - size);
                 double deltaX = (dashSize)/(Math.sqrt(1 + m * m));
                 double deltaY = (dashSize * m)/(Math.sqrt(1 + m * m));
-                double x1 = xScaler * ((xMid - deltaX) - xMinSet);
-                double y1 = -(yScaler * ((yMid - deltaY) - yMinSet) - size);
-                double x2 = xScaler * ((xMid + deltaX) - xMinSet);
-                double y2 = -(yScaler * ((yMid + deltaY) - yMinSet) - size);
+                double x1 = xMid - deltaX;
+                double y1 = yMid + deltaY;
+                double x2 = xMid + deltaX;
+                double y2 = yMid - deltaY;
                 g.setColor(Color.BLACK);
                 g.drawLine((int)(x1), (int)(y1), (int)(x2), (int)(y2));
             }
         }
     }
-
-  /*  private void draw(Graphics g, double xMinSet, double yMinSet, double xMaxSet, double yMaxSet, int stepSet){
-        double xIncrement = (xMaxSet - xMinSet)/stepSet;
-        double yIncrement = (yMaxSet - yMinSet)/stepSet;
-        double dashSize = 10;
-        for (int i = 0; i < stepSet; i++){
-            for (int j = 0; j < stepSet; j++){
-                double xMid = xMinSet + (0.5 + i) * xIncrement;
-                double yMid = yMinSet + (0.5 + j) * yIncrement;
-                m = xMid*xMid-yMid;
-                double deltaX = (dashSize)/(Math.sqrt(1 + m * m));
-                double deltaY = (dashSize * m)/(Math.sqrt(1 + m * m));
-                double x1 = xScaler * ((xMid - deltaX) - xMinSet);
-                double y1 = -(yScaler * ((yMid - deltaY) - yMinSet) - size);
-                double x2 = xScaler * ((xMid + deltaX) - xMinSet);
-                double y2 = -(yScaler * ((yMid + deltaY) - yMinSet) - size);
-                g.setColor(Color.BLACK);
-                g.drawLine((int)(x1), (int)(y1), (int)(x2), (int)(y2));
-            }
-        }
-    }
-    */
 }
 
 
