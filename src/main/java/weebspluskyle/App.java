259885c3c5c7ef14666851c8eb7a62bd3cef3810
Kyle Le <>
2023-03-20 04:39:35 +0000

    probably won't keep, let's see how this works tho used gridbag now

--- src/main/java/weebspluskyle/App.java
+++ src/main/java/weebspluskyle/App.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import javax.swing.*;
 
 public class App {
+    private static final Insets insets = new Insets(5, 5, 5, 5);
+    
     private static void createAndShowGUI() {
         JFrame jFrame = new JFrame("Slope Field Generator");
         jFrame.setLayout(null);
@@ -11,66 +13,65 @@ public class App {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel label = new JLabel("Slope Field Title");
-        label.setBounds(200, 20, 200, 20);
-        jFrame.add(label);
+        addComponent(jFrame, label, 3, 0, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
+        
         JLabel dytxt = new JLabel("DY");
-        dytxt.setBounds(20, 100, 50, 20);
-        jFrame.add(dytxt);
+        addComponent(jFrame, dytxt, 0, 1, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+        
 
         JLabel line = new JLabel("_____ =");
-        line.setBounds(14, 103, 50, 20);
-        jFrame.add(line);
+        addComponent(jFrame, line, 0, 2, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
         JLabel dxtxt = new JLabel("DX");
-        dxtxt.setBounds(20, 120, 50, 20);
-        jFrame.add(dxtxt);
+        addComponent(jFrame, dxtxt, 0, 3, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
         JTextField tf = new JTextField(20);
-        tf.setBounds(65, 105, 220, 30);
         TextPrompt tpEquation = new TextPrompt("Enter your differential equation", tf);
         tpEquation.changeAlpha(170);
-        jFrame.add(tf);
+        addComponent(jFrame, tf, 3, 2, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
+        
         JTextField xMin = new JTextField(10);
-        xMin.setBounds(65, 150, 100, 30);
         TextPrompt tpXMin = new TextPrompt("X Min", xMin);
         tpXMin.changeAlpha(170);
-        jFrame.add(xMin);
+        addComponent(jFrame, xMin, 0, 5, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
         JTextField xMax = new JTextField(10);
-        xMax.setBounds(180, 150, 100, 30);
         TextPrompt tpXMax = new TextPrompt("X Max", xMax);
         tpXMax.changeAlpha(170);
-        jFrame.add(xMax);
+        addComponent(jFrame, xMax, 3, 5, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
         JTextField yMin = new JTextField(10);
-        yMin.setBounds(65, 190, 100, 30);
         TextPrompt tpYMin = new TextPrompt("Y Min", yMin);
         tpYMin.changeAlpha(170);
-        jFrame.add(yMin);
+        addComponent(jFrame, yMin, 0, 7, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+
 
         JTextField yMax = new JTextField(10);
-        yMax.setBounds(180, 190, 100, 30);
         TextPrompt tpYMax = new TextPrompt("Y Max", yMax);
         tpYMax.changeAlpha(170);
-        jFrame.add(yMax);
+        addComponent(jFrame, yMax, 3, 7, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
         JTextField stepSet = new JTextField();
-        stepSet.setBounds(130, 230, 80, 30);
         TextPrompt tpGap = new TextPrompt("Gap Size", stepSet);
         tpGap.changeAlpha(170);
-        jFrame.add(stepSet);
+        addComponent(jFrame, stepSet, 1, 9, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+        
+        Button devs = new Button("Meet the Devs of Weebs + Kyle");
+        addComponent(jFrame, devs, 1, 12, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+        
+        devs.addActionListener(e -> {
+            Devs.newScreen();
+        });
 
-        Button b = new Button("Submit");
-        b.setBounds(40, 280, 100, 30);
-        jFrame.add(b);
+        Button submit = new Button("Submit");
+        addComponent(jFrame, submit, 1, 11, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 
         Graph graph = new Graph(300, 70, 300);
-        jFrame.add(graph);
-
-        // b button listener
-        b.addActionListener(e -> {
+        addComponent(jFrame, graph, 7, 4, 6, 6, GridBagConstraints.CENTER, GridBagConstraints.BOTH);        
+        
+        submit.addActionListener(e -> {
             String rawExpression = tf.getText();
             double xMinSet = Double.parseDouble(xMin.getText());
             double yMinSet = Double.parseDouble(yMin.getText());
@@ -87,18 +88,16 @@ public class App {
             }
         });
 
-        Button c = new Button("Meet the Devs of Weebs + Kyle");
-        c.setBounds(40, 310, 250, 30);
-        jFrame.add(c);
-
-        c.addActionListener(e -> {
-            Devs.newScreen();
-        });
-
+        
         jFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
         createAndShowGUI();
     }
+    private static void addComponent(Container container, Component component, int gridx, int gridy,
+      int gridwidth, int gridheight, int anchor, int fill) {
+    GridBagConstraints gbc = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, 1.0, 1.0, anchor, fill, insets, 0, 0);
+    container.add(component, gbc);
+  }
 }
